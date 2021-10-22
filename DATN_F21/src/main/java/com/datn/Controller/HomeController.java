1df45f07c24958c6e80f30c0a798a74035bcd8a8
khangpc01094 <khangvhpc01094@fpt.edu.vn>
2021-10-22 07:53:20 +0700

    new Project DATN_F21

---
+++ DATN_F21/src/main/java/com/datn/Controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.datn.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	
+	@GetMapping("/form")
+	public String getform() {
+		return "/viewsUser/list";
+	}
+	
+	@GetMapping("/formAd")
+	public String getformAd() {
+		return "/viewsAdmin/list";
+	}
+}
---
+++ DATN_F21/src/main/java/com/datn/DatnF21Application.java
@@ -0,0 +1,13 @@
+package com.datn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnF21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatnF21Application.class, args);
+	}
+
+}
