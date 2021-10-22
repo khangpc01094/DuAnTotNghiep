0b233d4d3bcfc47798ea2afe2170ca6ebba09722
trungtmpc01115 <trungtmpc01115@fpt.edu.vn>
2021-10-22 21:43:28 +0700

    Trung add giao dien

--- DATN_F21/src/main/java/com/datn/Controller/HomeController.java
+++ DATN_F21/src/main/java/com/datn/Controller/HomeController.java
@@ -16,4 +16,34 @@ public class HomeController {
 	public String getformAd() {
 		return "/viewsAdmin/list";
 	}
+	
+	@GetMapping("/menu")
+	public String getMenu() {
+		return "/viewsUser/menu";
+	}
+	
+	@GetMapping("/login")
+	public String getLogin() {
+		return "/viewsUser/login";
+	}
+	
+	@GetMapping("/register")
+	public String getRegister() {
+		return "/viewsUser/register";
+	}
+	
+	@GetMapping("/my_account")
+	public String getMyAccount() {
+		return "/viewsUser/my_account";
+	}
+	
+	@GetMapping("/contact")
+	public String getContact() {
+		return "/viewsUser/contact";
+	}
+	
+	@GetMapping("/fogot_password")
+	public String getFogotPassword() {
+		return "/viewsUser/fogot_password";
+	}
 }
