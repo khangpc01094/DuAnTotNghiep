d38012ade579a7dc3b398b9757305d786284b408
phuongpc01043 <phuongttypc01043@fpt.edu.vn>
2021-10-23 22:25:34 +0700

    Phuong update view Nguoi ban

--- DATN_F21/src/main/java/com/datn/Controller/HomeController.java
+++ DATN_F21/src/main/java/com/datn/Controller/HomeController.java
@@ -5,15 +5,49 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
 	
-	@GetMapping("/form")
-	public String getform() {
-		return "/viewsUser/list";
+	@GetMapping("/formSeller")
+	public String getformSeller() {
+		return "/viewsSeller/allProduct";
 	}
 	
-	@GetMapping("/formAd")
-	public String getformAd() {
-		return "/viewsAdmin/list";
+	@GetMapping("/viewsSeller/allProduct")
+	public String getformAll() {
+		return "/viewsSeller/allProduct";
+	}
+	
+	@GetMapping("/viewsSeller/createProduct")
+	public String getformCreate() {
+		return "/viewsSeller/createProduct";
+	}
+	
+	@GetMapping("/viewsSeller/wait")
+	public String getformWait() {
+		return "/viewsSeller/wait";
+	}
+	
+	@GetMapping("/viewsSeller/sent")
+	public String getformSent() {
+		return "/viewsSeller/sent";
+	}
+	
+	@GetMapping("/viewsSeller/cancel")
+	public String getformCancel() {
+		return "/viewsSeller/cancel";
+	}
+	
+	@GetMapping("/viewsSeller/statisCate")
+	public String getformstatisCate() {
+		return "/viewsSeller/statisCate";
+	}
+	
+	@GetMapping("/viewsSeller/statisInvoice")
+	public String getformstatisInvoice() {
+		return "/viewsSeller/statisInvoice";
+	}
+	
+	@GetMapping("/viewsSeller/detail")
+	public String getformDetail() {
+		return "/viewsSeller/detail";
 	}
 }
