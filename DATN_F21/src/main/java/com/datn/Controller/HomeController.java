36240aad891fcefb58a916dcd4e4ac0046b714e5
khangpc01094 <khangvhpc01094@fpt.edu.vn>
2021-10-24 14:11:21 +0700

    Update Homcontroller

--- DATN_F21/src/main/java/com/datn/Controller/HomeController.java
+++ DATN_F21/src/main/java/com/datn/Controller/HomeController.java
@@ -31,5 +31,29 @@ public class HomeController {
 		return "/viewsUser/account";
 	}
 	
+	@GetMapping("/login")
+	public String getLogin() {
+		return "/viewsUser/login";
+	}
 	
+	@GetMapping("/register")
+	public String getRegister() {
+		return "/viewsUser/register";
+	}
+	
+	@GetMapping("/my_account")
+	public String getMyAccount() {
+		return "/viewsUser/my_account";
+	}
+	
+	@GetMapping("/contact")
+	public String getContact() {
+		return "/viewsUser/contact";
+	}
+	
+	@GetMapping("/fogot_password")
+	public String getFogotPassword() {
+		return "/viewsUser/fogot_password";
+	}
+
 }
