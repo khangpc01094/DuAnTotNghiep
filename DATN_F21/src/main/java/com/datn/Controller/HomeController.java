3db917f12db50577e84abf3244cce2e35e70ecb3
khangpc01094 <khangvhpc01094@fpt.edu.vn>
2021-11-08 21:37:57 +0700

    update project

--- DATN_F21/src/main/java/com/datn/Controller/HomeController.java
+++ DATN_F21/src/main/java/com/datn/Controller/HomeController.java
@@ -26,18 +26,6 @@ public class HomeController {
 		return "/viewsUser/checkout";
 	}
 
-	
-<<<<<<< HEAD
-	/*@GetMapping("/form")
-	
-	 * public String getform() { return "/viewsUser/list"; }
-	 * 
-	 * @GetMapping("/formAd") public String getformAd() { return "/viewsAdmin/list";
-	 * }
-	 * 
-	 * @GetMapping("/menu") public String getMenu() { return "/viewsUser/menu"; }
-	 */
-	
 	@GetMapping("/login")
 	public String getLogin() {
 		return "/viewsUser/login";
@@ -46,31 +34,6 @@ public class HomeController {
 	@GetMapping("/register")
 	public String getRegister() {
 		return "/viewsUser/register";
-	}
-	
-	@GetMapping("/my_account")
-	public String getMyAccount() {
-		return "/viewsUser/my_account";
-	}
-	
-	@GetMapping("/contact")
-	public String getContact() {
-		return "/viewsUser/contact";
-	}
-	
-	@GetMapping("/fogot_password")
-	public String getFogotPassword() {
-		return "/viewsUser/fogot_password";
-=======
-	@GetMapping("/login")
-	public String getLogin() {
-		return "/viewsUser/login";
-	}
-	
-	@GetMapping("/register")
-	public String getRegister() {
-		return "/viewsUser/register";
->>>>>>> develop
 	}
 	
 	@GetMapping("/my_account")
